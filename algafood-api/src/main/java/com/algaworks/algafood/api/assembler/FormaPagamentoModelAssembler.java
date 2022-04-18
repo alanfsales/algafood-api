e6a2782798f5e796923117b83f90a87700e98037
Alan Sales <alanfsales27@gmail.com>
2022-04-17 22:05:59 -0300

    até a aula 12.13

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +21,7 @@ public class FormaPagamentoModelAssembler {
 		return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
 	}
 	
-	public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formasPagamentos) {
+	public List<FormaPagamentoModel> toCollectionModel(Collection<FormaPagamento> formasPagamentos) {
 		return formasPagamentos.stream().map(formaPamento -> toModel(formaPamento))
 				.collect(Collectors.toList());
 	}
