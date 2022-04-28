f57d3b5ab72b83d33da377f631a64369ef033ca4
Alan Sales <alanfsales27@gmail.com>
2022-04-28 18:38:49 -0300

    até a aula 14.26

---
+++ algafood-api/src/main/java/com/algaworks/algafood/intrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,58 @@
+package com.algaworks.algafood.intrastructure.service.storage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
+
+import com.algaworks.algafood.core.storage.StorageProperties;
+import com.algaworks.algafood.domain.service.FotoStorageService;
+
+public class LocalFotoStorageService implements FotoStorageService {
+
+	@Autowired
+	private StorageProperties storageProperties;
+	
+	@Override
+	public FotoRecuperada recuperar(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+			
+			FotoRecuperada fotoRecuperada = FotoRecuperada.builder()
+					.inputStream(Files.newInputStream(arquivoPath)).build();
+			
+			return fotoRecuperada;
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível recuperar o arquvo", e);
+		}
+	}
+	
+	@Override
+	public void armazenar(NovaFoto novaFoto) {
+		try {
+			Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+					
+			FileCopyUtils.copy(novaFoto.getInputStream(), 
+					Files.newOutputStream(arquivoPath));
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível armazenar o arquvo", e);
+		}
+	}
+	
+	@Override
+	public void remover(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+			
+			Files.deleteIfExists(arquivoPath);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir o arquvo", e);
+		}
+	}
+	
+	private Path getArquivoPath(String nomeArquivo) {
+		return storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
+	}
+
+}
