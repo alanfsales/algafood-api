32de56cae0220ee10c856d4512f4d68683448d30
Alan Sales <alanfsales27@gmail.com>
2022-05-15 15:50:42 -0300

    até a aula 15.14

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.repository.PedidoRepository;
 
 @Service
 public class FluxoPedidoService {
@@ -13,16 +14,23 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		pedidoRepository.save(pedido);
 	}
 	
 	@Transactional
 	public void cancelar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.cancelar();
+		
+		pedidoRepository.save(pedido);
 	}
 	
 	@Transactional
---
+++ algafood-api/src/main/java/com/algaworks/algafood/intrastructure/service/email/EmailException.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.intrastructure.service.email;
+
+public class EmailException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmailException(String message) {
+		super(message);
+	}
+	
+	public EmailException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
